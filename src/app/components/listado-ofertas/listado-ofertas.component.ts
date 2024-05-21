ff0f58811017c170eeaae956c2c88fc7123552b3
Frida Ximena Ortiz Vázquez <98490968+Frida-Ortiz@users.noreply.github.com>
2024-05-20 20:58:25 -0600

    Carpetas listado de ofertas y boton

---
+++ src/app/components/listado-ofertas/listado-ofertas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-ofertas',
+  standalone: true,
+  imports: [],
+  templateUrl: './listado-ofertas.component.html',
+  styleUrl: './listado-ofertas.component.css'
+})
+export class ListadoOfertasComponent {
+
+}
