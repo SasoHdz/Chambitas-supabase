2dcb921f3eef4c74a7f29616475e59897e042b35
Frida Ximena Ortiz Vázquez <98490968+Frida-Ortiz@users.noreply.github.com>
2024-06-03 16:14:37 -0600

    Carpeta contratar

---
+++ src/app/components/contratar/contratar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contratar',
+  standalone: true,
+  imports: [],
+  templateUrl: './contratar.component.html',
+  styleUrl: './contratar.component.css'
+})
+export class ContratarComponent {
+
+}
